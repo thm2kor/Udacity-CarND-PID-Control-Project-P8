6068961faa211708e822a1cea3cebabd942225e7
thm2kor <thiyagarajan.masilamani@gmail.com>
2021-03-09 12:31:24 +0100

    included x and y axis labels

--- src/plotter.py
+++ src/plotter.py
@@ -34,8 +34,12 @@ def plot_cte_from_multiple_files(data_files):
     for i in range(index):
         #prepare the legends
         name = data_files[i][:-4].split('_')
-        label = 'Kp = {:.2f} Ki = {:.4f} Kd = {:.2f} MSE = {:.4f}'.format(float(name[1]),float(name[2]),float(name[3]), mse[i]) #Kp = {:4.2f} Ki = {:6.5f} Kd = {:4.2f} MSE: {:4.3}'.format(i)
+        label = 'Kp = {:.2f} Ki = {:.5f} Kd = {:.2f} MSE = {:.4f}'.format(float(name[1]),float(name[2]),float(name[3]), mse[i]) #Kp = {:4.2f} Ki = {:6.5f} Kd = {:4.2f} MSE: {:4.3}'.format(i)
         plt.plot (cte[i], label=label, linewidth=5)
+    # Set the x axis label of the current axis.
+    plt.xlabel('Time steps',  fontsize = 24)
+    # Set the y axis label of the current axis.
+    plt.ylabel('Cross Track Error' , fontsize = 24)
     plt.title('Cross Track Error - CTE', fontsize = 24)
     plt.grid(b=True, which='major', color='#666666', linestyle='-', alpha=0.6)
     plt.minorticks_on()
@@ -47,22 +51,16 @@ def plot_cte_from_multiple_files(data_files):
 
 
 if __name__ == '__main__':
-
-    #data_files = ['output_0.1_0_0.txt','output_0.2_0_0.txt','output_0.08_0_0.txt',
-    #             'output_0.09_0_0.txt','output_0.11_0_0.txt','output_0.12_0_0.txt',
-    #             'output_0.13_0_0.txt', 'output_0.15_0_0.txt', 'output_0_0_0.txt']
-    #data_files = [ 'output_0.1_0_0.txt', 'output_0.2_0_0.txt' ]
-    #data_files = ['output_0.11_0_0.txt','output_0.12_0_0.txt',
-    #             'output_0.13_0_0.txt', 'output_0.15_0_0.txt' ]
-    #data_files = ['output_0.08_0_0.txt', 'output_0.09_0_0.txt', 'output_0.1_0_0.txt']
-    #data_files = ['output_0.1_0_0.1.txt','output_0.1_0_0.5.txt',
-    #             'output_0.1_0_1.0.txt', 'output_0.1_0_2.0.txt' ]
-    data_files = ['output_0.15_0.0001_3.0.txt','output_0.1_0.00001_3.0.txt',
-                 'output_0.1_0.0002_3.0.txt' ]
-    #data_files = ['output_0.1_0_2.70.txt','output_0.1_0_2.75.txt',
-    #             'output_0.1_0_2.80.txt', 'output_0.1_0_2.85.txt' ]
-    #data_files = ['output_0.1_0_2.75.txt', 'output_0.1_0.00001_2.75.txt']
-    # data_files = ['output_0.1_0.00001_2.75.txt', 'output_0.1_0.0001_2.75.txt','output_0.1_0.0002_2.75.txt']
-    #data_files = ['output_0.15_0_0.txt', 'output_0.15_0_3.0.txt', 'output_0.15_0.0001_3.0.txt']
-    # data_files = ['output_0_0_0.txt']
+    ## datafile for step 1:
+    #data_files = ['output_0_0_0.txt']
+    
+    ## datafile for step 2:
+    #data_files = ['output_0.1_0_0.txt', 'output_0.2_0_0.txt', 'output_0.15_0_0.txt']
+    
+    ## datafile for step 3:
+    #data_files = ['output_0.15_0_2.0.txt', 'output_0.15_0_2.5.txt', 'output_0.15_0_2.75.txt', 'output_0.15_0_3.0.txt']
+    
+    ## datafile for step 4:
+    data_files = ['output_0.15_0.0001_3.0.txt', 'output_0.15_0.0002_3.0.txt', 'output_0.15_0.00001_3.0.txt']
+    
     plot_cte_from_multiple_files(data_files)
